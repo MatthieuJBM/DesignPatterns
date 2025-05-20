3d4929b2e5974a069e9980553588c904ea76cb0d
Mateusz Baryla <m.baryla@protonmail.ch>
2025-05-20 14:15:22 +0200

    Facade Lab - Konto.

---
+++ Facade_Lab/src/Konto.java
@@ -0,0 +1,24 @@
+public class Konto
+{
+  @SuppressWarnings("unused")
+  private String haslo;
+  String login;
+  
+  public Konto(String login)
+  {
+	 this.login = login;
+	 
+  }
+  
+  public void setHaslo(String haslo)
+  {
+	  this.haslo = haslo;
+	 
+  }
+  
+  public String getLogin()
+  {
+	  return login;
+  }
+	
+}
---
+++ Facade_Lab/src/Program.java
@@ -0,0 +1,21 @@
+public class Program
+{
+    Fasada api;
+    
+    public Program()
+    {
+      api = new Fasada("Jan", "Pty�", 1993, "studentx");
+      api.wypiszDzisiejszaDate();
+      api.wypiszLogin();
+      api.wypiszDane();
+      api.wypiszWiek();
+      
+    }
+    	
+	
+	public static void main(String[] args) 
+	{
+	  new Program();
+	}
+
+}
