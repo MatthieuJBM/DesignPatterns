5f9314daaddbacbe8b28f83d67c83d41d37cbbcf
Mateusz Baryla <m.baryla@protonmail.ch>
2025-05-21 21:31:43 +0200

    Abstract Factory & Singleton Lab 3 Zad 2

---
+++ Lab3_Zad2/src/Factory1.java
@@ -0,0 +1,5 @@
+public class Factory1 implements AbstractFactory {
+    public String createProduct() {
+        return "Product from Factory 1";
+    }
+}
---
+++ Lab3_Zad2/src/Factory3.java
@@ -0,0 +1,5 @@
+public class Factory3 implements AbstractFactory {
+    public String createProduct() {
+        return "Product from Factory 3";
+    }
+}
